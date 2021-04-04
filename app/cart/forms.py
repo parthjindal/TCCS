8a5e9a43341892cb43be2a3207c425dc57e37e63
Parth Jindal <pmjindal@gmail.com>
2021-04-04 05:37:14 +0530

    cart check

--- app/cart/forms.py
+++ app/cart/forms.py
@@ -1,39 +1,65 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField
-from wtforms.validators import DataRequired, Required, Email, EqualTo, ValidationError, Length, NumberRange, Optional
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField, FloatField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length, NumberRange, Optional
 from app.models import Address, Office
 
 
 class ConsignmentForm(FlaskForm):
-    volume = IntegerField("Volume")
-    destinationBranch = SelectField("Branch", coerce=int)
+    '''
 
-    senderCity = StringField("City", validators=[DataRequired()])
-    senderAddrLine = StringField("Address Line", validators=[
-                                 DataRequired(), Length(max=60)])
-    senderZipCode = StringField("Zip Code", validators=[
-                                DataRequired(), Length(min=6, max=6)])
-    receiverCity = StringField("City", validators=[DataRequired()])
-    receiverAddrLine = StringField("Address Line", validators=[
-                                   DataRequired(), Length(max=60)])
-    receiverZipCode = StringField("Zip Code", validators=[
-                                  DataRequired(), Length(min=6, max=6)])
+    '''
+    volume = IntegerField("Volume", validators=[DataRequired(), NumberRange(min=1)])
+
+    sAddrLine = StringField("Address Line", validators=[DataRequired(), Length(max=60)])
+    sCity = StringField("City", validators=[DataRequired()])
+    sZipCode = StringField("Zip Code", validators=[DataRequired(), Length(min=6, max=6)])
+
+    rAddrLine = StringField("Address Line", validators=[DataRequired(), Length(max=60)])
+    rCity = StringField("City", validators=[DataRequired()])
+    rZipCode = StringField("Zip Code", validators=[DataRequired(), Length(min=6, max=6)])
+
+    branch = SelectField("Branch", coerce=int)
+
     submit = SubmitField("Create")
 
     def __init__(self, **kwargs):
+        '''
+
+        '''
         super().__init__(**kwargs)
-        # print("somewhers")
-        self.destinationBranch.choices = [
-            (x.id, x.name) for x in Office.query.order_by("name")]
-        print([x.id, x.name] for x in Office.query.order_by("name"))
+        self.branch.choices = [(x.id, f'{x.address.city} Office')
+                               for x in Office.query.order_by("id")]
+
+    def validate_rZipCode(self, field):
+        '''
+
+        '''
+        try:
+            int(field.data)
+        except ValueError:
+            raise ValidationError("All field-chars must be between 0-9")
+
+    def validate_sZipCode(self, field):
+        '''
+
+        '''
+        try:
+            int(field.data)
+        except ValueError:
+            raise ValidationError("All field-chars must be between 0-9")
+
 
 class TruckForm(FlaskForm):
-    plateNo = StringField("Plate No.")
+    '''
+
+    '''
+    plateNo = StringField("Plate No.", validators=[DataRequired()])
     branch = SelectField("Branch", coerce=int)
     submit = SubmitField("Create")
 
     def __init__(self, **kwargs):
+        '''
+        '''
         super().__init__(**kwargs)
-        self.branch.choices = [
-            (x.id, x.name) for x in Office.query.order_by("name")]
-        # print([x.id, x.name] for x in Office.query.order_by("name"))
+        self.branch.choices = [(x.id, f'{x.address.city} Office')
+                               for x in Office.query.order_by("id")]
--- app/cart/routes.py
+++ app/cart/routes.py
@@ -1,101 +1,106 @@
-from app.models.office import BranchOffice
 from app.cart import cart
-from .forms import ConsignmentForm, TruckForm
-from flask import redirect, url_for
+from flask import redirect, url_for, flash, current_app, request, render_template
 from flask_login import current_user, login_required
-from flask import request, render_template
-from app.models import Office, Consignment, Address, Truck, Manager
 from app import db, mail
-from flask import flash
+from app.models import Office, Consignment, Address, Truck, Manager
+from .forms import ConsignmentForm, TruckForm
 from flask_mail import Message
-from flask import current_app
 
 
-@cart.route("/")
+@cart.route("/place", methods=["GET"])
 @login_required
 def index():
+    '''
+
+    '''
     if current_user.role == "employee":
-        branch = Office.query.get(current_user.branchID).name
-        return redirect(url_for("cart.place", branch=branch, id=current_user.id))
+
+        office = Office.query.get(current_user.branchID)
+        office_addr = f'{office.address.city}-office'
+        return redirect(url_for("cart.place", office=office_addr, id=office.id))
+
     else:
-        flash("Access Denied")
+
+        flash("Access Denied", 'warning')
         return redirect(url_for("main.home"))
 
 
-@cart.route("/place/<branch>/<id>", methods=["GET", "POST"])
+@cart.route("/place/<office>/<id>/", methods=["GET", "POST"])
 @login_required
-def place(branch, id):
+def place(office, id):
+    '''
+
+    '''
     form = ConsignmentForm()
     if form.validate_on_submit():
+
         senderAddress = Address(addrLine=form.senderAddrLine.data,
-                                city=form.senderCity.data, zipCode=form.senderZipCode.data)
-        receiverAddress = Address(addrLine=form.receiverAddrLine.data,
-                                  city=form.receiverCity.data, zipCode=form.receiverZipCode.data)
+                                city=form.sCity.data, zipCode=form.sZipCode.data)
+        receiverAddress = Address(addrLine=form.rAddrLine.data,
+                                  city=form.rCity.data, zipCode=form.rZipCode.data)
         consign = Consignment(
             volume=form.volume.data, senderAddress=senderAddress, receiverAddress=receiverAddress,
-            dstBranchId=form.destinationBranch.data, srcBranchId=current_user.branchID)
+            dstBranchID=form.branch.data, srcBranchID=office.id)
+
         db.session.add(consign)
         db.session.commit()
-        allotTruck(consign, BranchOffice.query.get(current_user.branchID))
-        flash("Consignment Placed for Delivery")
+
+        flash("Consignment Placed for Delivery", 'info')
         return redirect(url_for("main.home"))
+
     return render_template("cart/place.html", title="Place Consignment", form=form)
 
-@cart.route("/request_truck", methods=['GET', 'POST'])
+
+@cart.route("/request-truck/", methods=['GET', 'POST'])
 @login_required
 def request_truck():
-    if current_user.is_authenticated and current_user.role == "manager":
-        flash('You are not authorized to access this page', 'success')
+    '''
+
+    '''
+    if current_user.role == "manager":
+        flash('Access Denied', 'warning')
         return redirect(url_for('main.home', role=current_user.role))
-    user = Manager.query.filter_by(role="manager").first()
-    brnch = Office.query.filter_by(id=current_user.branchID).first()
+
+    manager = Manager.query.filter_by(role="manager").first()
+    office = Office.query.filter_by(id=current_user.branchID).first()
+
     msg = Message('Buy New Truck Request',
                   sender=current_app.config['MAIL_USERNAME'],
-                  recipients=[user.email])
-    msg.body = f'''Hi {user.name}
-    
-An employee at Egret {current_user.name} requested for adding truck to the branch {brnch.name}.
-
-To buy a new truck for the branch, please go to the following web address:
-{url_for('cart.addTruck', _external=True)}
-
-To check the statistics of the branches, please visit:
-{url_for('main.branches', _external=True)}
-
-Team Egret
-'''
+                  recipients=[manager.email],
+                  body=render_template("request", user=manager, branch=office))
     mail.send(msg)
-    flash('The request has been sent to the manager', 'success')
+
+    flash('Request mail sent!', 'success')
     return redirect(url_for('main.home', role=current_user.role))
 
-@cart.route("/addTruck", methods=["GET", "POST"])
+
+@cart.route("/add-truck/", methods=["GET", "POST"])
 @login_required
 def addTruck():
-    if current_user.is_authenticated and current_user.role == "manager":
+    '''
+
+    '''
+    if current_user.role == "employee":
+
+        flash("Access Denied", "warning")
+        return redirect(url_for("main.home"), role=current_user.role)
+
+    if current_user.role == "manager":
+
         form = TruckForm()
         if form.validate_on_submit():
-            trck = Truck(
-                plateNo=form.plateNo.data, usageTime=0, idleTime=0)
-            brnch = BranchOffice.query.get(form.branch.data)
-            # print(brnch)
-            db.session.add(trck)
+
+            truck = Truck(plateNo=form.plateNo.data, branchID=form.branch.data)
+            db.session.add(truck)
             db.session.commit()
-            brnch.addTruck(trck)
-            flash("Truck has been added")
+
+            flash("Truck Added", 'success')
             return redirect(url_for("main.home"))
+
         return render_template("cart/addTruck.html", title="Buy new truck", form=form)
-    flash('You are not authorized to access this page', 'warning')
-    return redirect(url_for('main.home', role=current_user.role))
 
 
-@cart.route("/view/branch", methods=["GET"])
-@login_required
-def view():
-    consignments = Consignment.query.filter_by(
-        srcBranchId=current_user.branchID)
-
-
-@cart.route("/receive/", methods=["GET", "POST"])
+@cart.route("/receive/truck", methods=["GET", "POST"])
 @login_required
 def receive():
     branchID = current_user.branchID
