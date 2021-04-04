f3a9edc6b25d863cc41f5f4b92353aca54375ade
Parth Jindal <pmjindal@gmail.com>
2021-04-04 22:42:14 +0530

    debug code

--- conftest.py
+++ conftest.py
@@ -6,20 +6,16 @@ from testConfig import TestConfig
 @pytest.fixture()
 def test_client():
     app = create_app(TestConfig)
-    with app.test_client() as client:
-        with app.app_context():
-            pass
-        yield client
+    testing_client = app.test_client()
+    ctx = app.app_context()
+    ctx.push()
+
+    yield testing_client
+    ctx.pop()
+
 
 @pytest.fixture()
 def database():
     db.create_all()
     yield db
     db.drop_all()
-
-@pytest.fixture
-def App():
-    app = create_app(TestConfig)
-    app.app_context().push()
-    yield app
-    app.app_context().pop()
